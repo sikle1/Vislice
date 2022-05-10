c0cf665658aa56a0d0fb7d2815cc1be5a67b6058
Domen Skornšek <domi.skornsek@gmail.com>
2022-05-10 19:12:40 +0200

    comit2

--- model.py
+++ model.py
@@ -6,6 +6,7 @@ STEVILO_DOVOLJENIH_NAPAK = 10
 PRAVILNA_CRKA = '+'
 PONOVLJENA_CRKA = 'o'
 NAPACNA_CRKA = '-'
+ZACETEK = '5'
 ZMAGA = 'W'
 PORAZ = 'X'
 
@@ -78,3 +79,25 @@ with open("besede.txt", encoding="utf_8") as f:
 
 def nova_igra():
     return Igra(random.choice(bazen_besed))
+
+
+class Vislice:
+    def __init__(self) -> None:
+        self.igre = {}
+
+    def prost_id_igre(self):
+        if len(self.igre) == 0:
+            return 0
+        else:
+            return max(self.igre.keys()) + 1
+    
+    def nova_igra(self):
+        id_igre = self.prost_id_igre()
+        igra = nova_igra()
+        self.igre[id_igre] = (igra, ZACETEK)
+        return id_igre
+
+    def ugibaj(self, id_igre, crka):
+        igra, _ = self.igre[id_igre]
+        stanje = igra.ugibaj(crka)
+        self.igre[id_igre] = (igra, stanje)
